f20def49cef8dda8cbffb0cd87dad686457e2aec
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 08:31:28 -0400

    Initial Commit

---
+++ sketch.js
@@ -0,0 +1,24 @@
+
+function setup() {
+  createCanvas(500, 500);
+  background(220, 220, 220);
+}
+
+
+
+
+function draw() {
+}
+
+function mouseClicked() {
+  // var r = random(0, 255)
+  // var g = random(0, 255)
+  // var b = random(0, 255)
+  // stroke(r, g, b)
+  background(220, 220, 220);
+}
+
+function draw(){
+  line(250, 250, mouseX, mouseY)
+}
+
